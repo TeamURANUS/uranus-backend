336a75aa515c74487b521103cc09453a43057223
arslanmustafabora <mboraarslan1999@gmail.com>
2022-03-06 02:40:11 +0300

    add calendar.js environment variables to project env to prevent .env error

--- utils/config.js
+++ utils/config.js
@@ -20,14 +20,20 @@ const {
     AUTH_URI,
     TOKEN_URI,
     AUTH_PROVIDER_X509_CERT_URL,
-    CLIENT_X509_CERT_URL
-
+    CLIENT_X509_CERT_URL,
+    CALENDAR_PRIVATE_KEY_ID,
+    CALENDAR_PRIVATE_KEY,
+    CALENDAR_CLIENT_EMAIL,
+    CALENDAR_CLIENT_ID,
+    CALENDAR_CLIENT_X509_CERT_URL,
+    CALENDAR_ID
 } = process.env;
 
 
 module.exports = {
     port: PORT,
     host: HOST,
+    calendarId: CALENDAR_ID,
     firebaseConfig: {
         apiKey: API_KEY,
         authDomain: AUTH_DOMAIN,
@@ -48,5 +54,17 @@ module.exports = {
         token_uri: TOKEN_URI,
         auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
         client_x509_cert_url: CLIENT_X509_CERT_URL
+    },
+    CREDENTIALS: {
+        type: TYPE,
+        project_id: PROJECT_ID,
+        private_key_id: CALENDAR_PRIVATE_KEY_ID,
+        private_key: CALENDAR_PRIVATE_KEY,
+        client_email: CALENDAR_CLIENT_EMAIL,
+        client_id: CALENDAR_CLIENT_ID,
+        auth_uri: AUTH_URI,
+        token_uri: TOKEN_URI,
+        auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: CALENDAR_CLIENT_X509_CERT_URL
     }
 }
